893274ad83c0cde2785872ef5e47a54b2a26c2c5
Zach Toogood <zrtoogood@gmail.com>
2023-08-10 20:53:59 +0100

    [sql] Add maxspawns to mob_spawn_slots

--- sql/mob_spawn_slots.sql
+++ sql/mob_spawn_slots.sql
@@ -6,11 +6,14 @@
 DROP TABLE IF EXISTS `mob_spawn_slots`;
 CREATE TABLE `mob_spawn_slots` (
   `id` int(10) unsigned NOT NULL,
-  `mobid` int(10) unsigned NOT NULL,
+  `zoneid` smallint(3) unsigned NOT NULL DEFAULT 0,
   `chance` tinyint(4) DEFAULT '0',
-  PRIMARY KEY (`id`, `mobid`)
+  `maxspawns` tinyint(4) DEFAULT '0',
+  PRIMARY KEY (`id`, `zoneid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
 LOCK TABLES `mob_spawn_slots` WRITE;
+
+-- TODO: Replace me with data
 
 UNLOCK TABLES;
